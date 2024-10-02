a994257c9a414c4638244dc53c7d11d031f538b6
mariano444 <80271490+mariano444@users.noreply.github.com>
2024-10-02 16:22:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
 import time
 import uuid
-import requests
+import logging
 from flask import Flask, render_template, redirect, url_for, flash, request
 from forms import PublicationForm
+from FacebookMarketplaceBot import FacebookMarketplaceBot
 from localidades import localidades_argentinas
 import random, os
 from werkzeug.utils import secure_filename
+from selenium.webdriver.chrome.service import Service
 
-# Configuración de la aplicación Flask
+# Configuración para Selenium en modo headless
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_headless_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/google-chrome-stable"  # Ajusta según la instalación de tu sistema
+
+    # Cambia a la ruta correcta donde está instalado tu chromedriver
+    chrome_driver_path = "/usr/bin/chromedriver"
+    service = Service(chrome_driver_path)
+    
+    # Inicializa el WebDriver
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
+
+# Configurar la aplicación Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['MODIFIED_UPLOAD_FOLDER'] = 'modified_uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Máximo 16 MB
 
-# Crear carpetas si no existen
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 os.makedirs(app.config['MODIFIED_UPLOAD_FOLDER'], exist_ok=True)
 
@@ -23,35 +44,39 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Ruta para el formulario
+# Logger para monitorear las publicaciones
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Global para controlar las fotos ya publicadas
+imagenes_publicadas = set()
+
+def formatear_descripcion(texto):
+    lineas = texto.split('\n')
+    lineas_limpias = [f'"{linea.strip()} \\n"' for linea in lineas if linea.strip()]
+    descripcion_formateada = 'description = (\n' + '    '.join(lineas_limpias) + '\n)'
+    return descripcion_formateada
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = PublicationForm()
     form.localidad.choices = [(loc, loc) for loc in localidades_argentinas]
 
     if form.validate_on_submit():
-        # Capturar los datos del formulario
         username = form.username.data
         password = form.password.data
         num_publications = form.num_publications.data
         selected_localities = form.localidad.data
-
-        # Capturar frases del usuario
         frases_usuario = form.phrases.data
-        frases_lista = [frase.strip() for frase in frases_usuario.split(",") if frase.strip()] 
+        frases_lista = [frase.strip() for frase in frases_usuario.split(",") if frase.strip()][:10] 
 
-        # Asegurar que no se excedan 10 frases
-        if len(frases_lista) > 10:
-            frases_lista = frases_lista[:10]
-
-        # Extraer datos del formulario
         marca = form.marca.data
         modelo = form.modelo.data
         precio = form.precio.data
         millaje = form.millaje.data
         descripcion = form.descripcion.data
+        descripcion_formateada = formatear_descripcion(descripcion)
 
-        # Manejar las imágenes subidas
         imagenes_subidas = request.files.getlist(form.imagenes.name)
         imagenes_guardadas = []
         for imagen in imagenes_subidas:
@@ -61,35 +86,73 @@ def index():
                 imagen.save(image_path)
                 imagenes_guardadas.append(image_path)
             else:
-                flash('Algunas imágenes no tienen un formato permitido. Solo se permiten archivos PNG, JPG, JPEG y GIF.', 'danger')
+                flash('Algunas imágenes no tienen un formato permitido.', 'danger')
                 return redirect(url_for('index'))
 
-        # Preparar los datos para enviarlos al bot local
-        data = {
-            'username': username,
-            'password': password,
-            'num_publications': num_publications,
-            'localities': selected_localities,
-            'frases': frases_lista,
-            'marca': marca,
-            'modelo': modelo,
-            'precio': precio,
-            'millaje': millaje,
-            'descripcion': descripcion
-        }
-
-        files = [('imagenes', (os.path.basename(image), open(image, 'rb'), 'image/jpeg')) for image in imagenes_guardadas]
-
+        # Crear el bot con navegador en modo headless
+        bot = FacebookMarketplaceBot(username, password, driver=get_headless_driver())
         try:
-            # Cambia localhost por la URL de tu túnel
-            response = requests.post('https://0da80c72f3e1a5566208fe7cd9bd2cdf.serveo.net/process', data=data, files=files)
-            if response.status_code == 200:
-                flash('Publicaciones realizadas con éxito!', 'success')
-            else:
-                flash(f'Error en el bot local: {response.text}', 'danger')
+            bot.login()
         except Exception as e:
-            flash(f'Error al enviar los datos al bot local: {str(e)}', 'danger')
+            flash(f'Error al iniciar sesión: {str(e)}', 'danger')
+            return redirect(url_for('index'))
 
+        localidades_asignadas = bot.assign_locations(num_publications, selected_localities)
+
+        for i in range(num_publications):
+            form_data = {
+                "Marca": marca,
+                "Modelo": modelo,
+                "Precio": precio,
+                "Millaje": millaje
+            }
+
+            assigned_location = localidades_asignadas[i]
+            imagenes_disponibles = list(set(imagenes_guardadas) - imagenes_publicadas)
+            if len(imagenes_disponibles) == 0:
+                imagenes_publicadas.clear()
+                imagenes_disponibles = imagenes_guardadas
+
+            primera_imagen = random.choice(imagenes_disponibles)
+            imagenes_publicadas.add(primera_imagen)
+
+            unique_id = str(uuid.uuid4())
+            filename, file_extension = os.path.splitext(os.path.basename(primera_imagen))
+            modified_image_path = os.path.join(app.config['MODIFIED_UPLOAD_FOLDER'], f"{filename}_modified_{unique_id}{file_extension}")
+
+            if not os.path.exists(modified_image_path):
+                try:
+                    modified_image_path = bot.modify_and_save_photo(primera_imagen, modified_image_path, frases_lista)
+                except Exception as e:
+                    flash(f'Error al modificar la imagen: {str(e)}', 'danger')
+                    continue
+
+            imagenes_restantes = [img for img in imagenes_guardadas if img != primera_imagen]
+            imagenes_para_publicar = [modified_image_path]
+
+            max_fotos_a_cargar = min(18, len(imagenes_restantes))
+            contador_imagenes = 1
+
+            while contador_imagenes < max_fotos_a_cargar + 1 and imagenes_restantes:
+                imagen_random = random.choice(imagenes_restantes)
+                imagenes_para_publicar.append(imagen_random)
+                imagenes_restantes.remove(imagen_random)
+                contador_imagenes += 1
+
+            try:
+                bot.complete_form(form_data, descripcion_formateada, imagenes_para_publicar, [assigned_location])
+                bot.click_button("Publicar")
+                logger.info(f"Publicación {i + 1}/{num_publications} completada.")
+                flash(f"Publicación {i + 1} completada.", 'success')
+            except Exception as e:
+                flash(f'Error al realizar la publicación {i + 1}: {str(e)}', 'danger')
+                logger.error(f"Error al realizar la publicación {i + 1}: {str(e)}")
+                continue
+
+            time.sleep(random.uniform(30, 60))
+
+        bot.close_browser()
+        flash('Publicaciones realizadas con éxito!', 'success')
         return redirect(url_for('index'))
 
     return render_template('index.html', form=form)
